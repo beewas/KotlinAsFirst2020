5edd68106581fee637279f379cf704b657f2c846
Nick <signaalwespbij@yandex.ru>
2020-09-12 17:44:00 +0300

    A few stylistic changes

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -65,7 +65,8 @@ fun main() {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = (minutes + hours * 60) * 60 + seconds
+fun seconds(hours: Int, minutes: Int, seconds: Int) =
+    (minutes + hours * 60) * 60 + seconds
 
 /**
  * Тривиальная (1 балл)
@@ -74,7 +75,8 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = (minutes + hours * 60
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = ((sagenes * 3 + arshins) * 16 + vershoks) * 0.04445
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int) =
+    ((sagenes * 3 + arshins) * 16 + vershoks) * 0.04445
 
 /**
  * Тривиальная (1 балл)
@@ -82,7 +84,8 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = ((sagene
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = (((deg * 60 + min) * 60 + sec) * PI / (180 * 60 * 60))
+fun angleInRadian(deg: Int, min: Int, sec: Int) =
+    ((deg * 60 + min) * 60 + sec) * PI / (180 * 60 * 60)
 
 /**
  * Тривиальная (1 балл)
@@ -90,7 +93,8 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double = (((deg * 60 + min) * 6
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(sqr(x1 - x2) + sqr(y1 - y2))
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double) =
+    sqrt(sqr(x1 - x2) + sqr(y1 - y2))
 
 /**
  * Простая (2 балла)
@@ -98,8 +102,7 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(s
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-// 48 - код нуля в Unicode
-fun thirdDigit(number: Int): Int = number.toString()[number.toString().length - 3].toInt() - 48
+fun thirdDigit(number: Int) = number % 1000 / 100
 
 /**
  * Простая (2 балла)
@@ -108,7 +111,8 @@ fun thirdDigit(number: Int): Int = number.toString()[number.toString().length - 
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = (hoursArrive - hoursDepart) * 60 + minutesArrive - minutesDepart
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int) =
+    (hoursArrive - hoursDepart) * 60 + minutesArrive - minutesDepart
 
 /**
  * Простая (2 балла)
@@ -117,15 +121,13 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double {
-    val per = percent / 100.0 + 1
-    return ((per * initial) * per) * per
-}
-
+fun accountInThreeYears(initial: Int, percent: Int) =
+    initial * (percent / 100.0 + 1).pow(3)
 /**
  * Простая (2 балла)
  *
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = (number % 10) * 100 + (number % 100 / 10) * 10 + number / 100
+fun numberRevert(number: Int) =
+    (number % 10) * 100 + (number % 100 / 10) * 10 + number / 100
