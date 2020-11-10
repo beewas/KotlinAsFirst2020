1e0d756930bab2ca28ffe01f51b612d5c699f9e1
beewas <saintpolytech@yandex.ru>
2020-11-10 16:37:59 +0300

    Indentation

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -279,7 +279,8 @@ fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> {
     if (list.size < 2) return Pair(-1, -1)
     val values = mutableMapOf<Int, Int>()
     for (i in list.indices) values[list[i]] = i
-    for (i in list.indices) if (number - list[i] in values && values[number - list[i]] != i) return Pair(values[number - list[i]]!!, i).sorted()
+    for (i in list.indices) if (number - list[i] in values && values[number - list[i]] != i)
+        return Pair(values[number - list[i]]!!, i).sorted()
     return Pair(-1, -1)
 }
 
